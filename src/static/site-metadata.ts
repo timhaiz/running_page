f8d16c2bb58b2b9d74c7f4ec22e6257f080f5e52
TimZhang <32918064+timhaiz@users.noreply.github.com>
2024-09-24 13:27:39 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,10 +15,7 @@ const data: ISiteMetadataResult = {
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
+
     {
       name: 'About',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
