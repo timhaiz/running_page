9ba1a10d709cc61609d946369ad38a94078fec8a
TimZhang <32918064+timhaiz@users.noreply.github.com>
2024-09-28 05:51:16 +0000

    update

--- run_page/icloud_update_to_fitout.py
+++ run_page/icloud_update_to_fitout.py
@@ -3,25 +3,35 @@ import os
 
 
 # 使用你的 Apple ID 和应用专用密码,认证到 iCloud
+email = os.environ['ICLOUD_EMAIL']
+password = os.environ['ICLOUD_PASSWORD']
 
 
+def authenticate_icloud(email, password):
+    api = PyiCloudService(email, password)
 
-def download_fit(email = os.environ['ICLOUD_EMAIL'], password = os.environ['ICLOUD_PASSWORD'] ):
-    api = PyiCloudService(os.environ['ICLOUD_EMAIL'], os.environ['ICLOUD_PASSWORD'] )
-    
+
+    return api
+
+
+def download_fit():
+
+    api = authenticate_icloud(email, password)
     try:
         # 使用 api.drive 来访问 iCloud Drive
         files = api.drive['Blackbird'].dir()
         for f in files:
-            print(f)
             if f.endswith(".fit"):
-                base_name = os.path.splitext(f)[0]  
-                fit_out_path = os.path.join("FIT_OUT", f)
+                # 使用 os.path.basename 获取文件名，避免路径问题
+                base_name = os.path.splitext(os.path.basename(f))[0]
+                # 使用 os.path.join 构建路径，确保路径正确
+                fit_out_path = os.path.join("FIT_OUT", os.path.basename(f))
                 lock_file_path = os.path.join("FIT_OUT", f"{base_name}.fit.lock")
-                if not os.path.exists(fit_out_path) and not os.path.exists(lock_file_path): 
+                if not os.path.exists(fit_out_path) and not os.path.exists(lock_file_path):
                     download = api.drive['Blackbird'][f].open(stream=True)
                     with open(fit_out_path, 'wb') as opened_file:
                         opened_file.write(download.raw.read())
+
 
         print('ok')
     except KeyError as e:
