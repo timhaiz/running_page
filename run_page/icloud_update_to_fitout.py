354d799ee5ee35570768d1e26ac5eb2670047227
timhai06 <timz@vip.qq.com>
2024-09-29 17:50:04 +0800

    update

--- run_page/icloud_update_to_fitout.py
+++ run_page/icloud_update_to_fitout.py
@@ -12,19 +12,14 @@ def download_fit():
         # 使用 api.drive 来访问 iCloud Drive
         files = api.drive['Blackbird'].dir()
         for f in files:
-            print(f)
-    #         if f.endswith(".fit"):
-    #             # 使用 os.path.basename 获取文件名，避免路径问题
-    #             base_name = os.path.splitext(os.path.basename(f))[0]
-    #             # 使用 os.path.join 构建路径，确保路径正确
-    #             fit_out_path = os.path.join("FIT_OUT", os.path.basename(f))
-    #             lock_file_path = os.path.join("FIT_OUT", f"{base_name}.fit.lock")
-    #             if not os.path.exists(fit_out_path) and not os.path.exists(lock_file_path):
-    #                 download = api.drive['Blackbird'][f].open(stream=True)
-    #                 with open(fit_out_path, 'wb') as opened_file:
-    #                     opened_file.write(download.raw.read())
-
-
+            if f.endswith(".fit"):
+                base_name = os.path.splitext(os.path.basename(f))[0]
+                fit_out_path = os.path.join("FIT_OUT", os.path.basename(f))
+                lock_file_path = os.path.join("FIT_OUT", f"{base_name}.fit.lock")
+                if not os.path.exists(fit_out_path) and not os.path.exists(lock_file_path):
+                    download = api.drive['Blackbird'][f].open(stream=True)
+                    with open(fit_out_path, 'wb') as opened_file:
+                        opened_file.write(download.raw.read())
     except KeyError as e:
         print("发生错误：", e)
     except Exception as e:
