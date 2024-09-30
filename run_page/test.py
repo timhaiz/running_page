61e0765d83d5120b008ec9336cd881f766afc034
timhai06 <timz@vip.qq.com>
2024-09-30 15:15:44 +0800

    update

--- run_page/test.py
+++ run_page/test.py
@@ -1,7 +1,7 @@
 import os
+from icloud_update_to_fitout import download_fit
 
-# 定义要修改的文件夹路径和文件名
-f_path = '/home/runner/work/running_page/running_page/FIT_OUT/sportRecord_20240927175050.fit'
-base_name = os.path.split(f_path)
-os.rename(base_name[1],base_name[1].join(".lock"))
-print(f_path)
+
+    
+if __name__ == '__main__':
+    download_fit()
